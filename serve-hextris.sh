a9ff6d56305a269497f9cad3ffb14a169d63b541
jaimee.taylor <jaimee.taylor@student.uq.edu.au>
2026-04-01 13:23:17 +1000

    Completed Prac

---
+++ serve-hextris.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+yum install -y httpd 
+systemctl enable httpd 
+systemctl start httpd 
+ 
+yum install -y git 
+cd /var/www/html 
+git clone https://github.com/Hextris/hextris .
+
+user_data = file("./serve-hextris.sh")
